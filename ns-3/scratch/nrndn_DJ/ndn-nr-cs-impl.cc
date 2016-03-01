04d246053ec9a0cbb22689dfca543308226c922c
JanDeng25 <1003932334@qq.com>
2016-03-01 16:02:28 +0800

    //NS_ASSERT_MSG(m_csContainer.size()!=0,"Empty cs container. No initialization?");

--- ns-3/scratch/nrndn_DJ/ndn-nr-cs-impl.cc
+++ ns-3/scratch/nrndn_DJ/ndn-nr-cs-impl.cc
@@ -122,7 +122,7 @@ NrCsImpl::Find (const Name &prefix)
 	//NS_ASSERT_MSG(false,"In NrCsImpl,NrCsImpl::Find (const Name &prefix) should not be invoked");
 	 NS_LOG_INFO ("Finding prefix"<<prefix.toUri());
 	 std::vector<Ptr<Entry> >::iterator it;
-	 NS_ASSERT_MSG(m_csContainer.size()!=0,"Empty cs container. No initialization?");
+	 //NS_ASSERT_MSG(m_csContainer.size()!=0,"Empty cs container. No initialization?");
 	 for(it=m_csContainer.begin();it!=m_csContainer.end();++it)
 	 {
 		 if((*it)->GetName()==prefix)
