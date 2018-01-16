38374e6553817032d57b61a800c3b9ed0f8f75c6
Jan Deng <1003932334@qq.com>
2018-01-16 15:50:26 +0800

    show detect packet & fib

--- ns-3/scratch/nrndn_dj/ndn-nr-fib-impl.cc
+++ ns-3/scratch/nrndn_dj/ndn-nr-fib-impl.cc
@@ -115,7 +115,7 @@ NrFibImpl::RemoveFromAll (Ptr<Face> face){
 void
 NrFibImpl::AddFibEntry (const Ptr<const Name> &prefix, std::string lane, std::pair<uint32_t, uint32_t> p)
 {
-	std::cout<<"add FIB Entry name:"<<prefix->toUri()<<" lane:"<<lane<<" TTL:"<<p.first<< ',' << p.second <<std::endl;
+	std::cout<<"add FIB Entry name:"<<prefix->toUri()<<" lane:"<<lane<<" TTL:"<<p.first<< '(' << p.second << ')' <<std::endl;
 	if(m_fibContainer.empty())
 	{
 		Ptr<EntryNrImpl> entry = ns3::Create<EntryNrImpl>(this,prefix,m_cleanInterval);
@@ -195,11 +195,12 @@ NrFibImpl::Print (std::ostream& os) const
 Ptr<Entry>
 NrFibImpl::Find (const Name &prefix)
 {
+	std::cout<<"Find FIB Entry name:"<<prefix->toUri() << std::endl;
 	//NS_ASSERT_MSG(false,"In NrFibImpl,NrFibImpl::Find (const Name &prefix) should not be invoked");
 	 NS_LOG_INFO ("Finding prefix"<<prefix.toUri());
 	 std::vector<Ptr<Entry> >::iterator it;
 	 //NS_ASSERT_MSG(m_fibContainer.size()!=0,"Empty fib container. No initialization?");
-	 for(it=m_fibContainer.begin();it!=m_fibContainer.end();++it)
+	 for(it=m_fibContainer.begin(); it!=m_fibContainer.end(); ++it)
 	 {
 		 if((*it)->GetPrefix()==prefix)
 		 {
