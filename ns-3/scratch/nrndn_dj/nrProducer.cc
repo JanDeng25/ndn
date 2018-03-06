e8f2fef0d0df73274f7f87a1011573b9094fb545
Jan Deng <1003932334@qq.com>
2018-03-06 20:31:29 +0800

    test resource

--- ns-3/scratch/nrndn_dj/nrProducer.cc
+++ ns-3/scratch/nrndn_dj/nrProducer.cc
@@ -98,9 +98,7 @@ void nrProducer::StartApplication()
 	{
 		//delay 50s to broadcast resource packet
 		//Simulator::Schedule (Seconds (50.0 + delay), &nrProducer::sendResourcePacket, this, i);
-		
-		//No
-		Simulator::Schedule (Seconds (delay), &nrProducer::sendResourcePacket, this, i);
+		Simulator::Schedule (Seconds (50.0 + delay), &nrProducer::sendResourcePacket, this, i);
 	}
 
 }
